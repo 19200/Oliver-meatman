f8d342a3b3d9d5def4b75b29d017a06f3001a0be
Jon20002179 <101559026+Jon20002179@users.noreply.github.com>
2023-12-05 23:08:16 +0000

    This should work
    yeheheheh

---
+++ Cards/ElvisSandwich.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+
+namespace MonsieurMeatman.Cards
+{
+    class ElvisSandwich : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            statModifiers.health = 1000f;
+            statModifiers.movementSpeed = 0.01f;
+            UnityEngine.Debug.Log($"[{MonsieurMeatman.ModInitials}][Card] {GetTitle()} has been setup.");
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[{MonsieurMeatman.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+            //Edits values on player when card is selected
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[{MonsieurMeatman.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+            //Run when the card is removed from the player
+        }
+
+
+        protected override string GetTitle()
+        {
+            return "Elvis Sandwich";
+        }
+        protected override string GetDescription()
+        {
+            return "Yeheheheheh";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Health",
+                    amount = "+100,000%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Speed",
+                    amount = "-99%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return MonsieurMeatman.ModInitials;
+        }
+    }
+}
+
---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using MonsieurMeatman.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace MonsieurMeatman
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class MonsieurMeatman : BaseUnityPlugin
+    {
+        private const string ModId = "com.MonsieurMeatman.Mod.Id";
+        private const string ModName = "MonsieurMeatman";
+        public const string Version = "0.0.1"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "MM";
+        public static MonsieurMeatman instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<ElvisSandwich>();
+        }
+    }
+}
